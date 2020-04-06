51489ae44d39b70c920bdd84c4a6ee1690451727
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-04-05 23:31:37 -0300

    Some configs

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { AppRegistry } from 'react-native';
-import App from './src';
+
 import { name as appName } from './app.json';
+import App from './src';
+import 'react-native-gesture-handler';
+
+require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+
+import { ThemeProvider } from 'styled-components';
+
+import light from '@styles/themes/light';
+
+import Routes from './routes';
 
 export default function src() {
-  return <View />;
+  return (
+    <ThemeProvider theme={light}>
+      <NavigationContainer>
+        <StatusBar
+          backgroundColor={light.colors.secondary}
+          barStyle='light-content'
+        />
+        <SafeAreaView
+          style={{ flex: 0, backgroundColor: light.colors.secondary }}
+        />
+        <Routes />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
 }
