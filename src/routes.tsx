b4d2f2376495fdf9b22f1697d8f3a73d3f30482a
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-04-10 15:03:16 -0300

    Improve routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,46 +1,51 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 
 import CustomDrawerContent from '@components/drawer_content';
-import About from '@screens/about';
 import CompanyDetails from '@screens/company_details';
 import CompanyList from '@screens/company_list';
-import LocationList from '@screens/location_list';
+import LocationList, { Location } from '@screens/location_list';
 
-export type RootDrawerParamList = {
+export type RootStackParamList = {
   LocationList: undefined;
-  CompanyList: { locationId: string };
-  About: undefined;
+  CompanyList: { location: Location };
   CompanyDetails: undefined;
 };
 
-const Drawer = createDrawerNavigator<RootDrawerParamList>();
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function Routes() {
+  function StackScreens() {
+    return (
+      <Stack.Navigator
+        headerMode='float'
+        screenOptions={{
+          headerShown: false,
+          animationEnabled: false,
+          gestureEnabled: false,
+        }}
+      >
+        <Stack.Screen name='LocationList' component={LocationList} />
+        <Stack.Screen name='CompanyList' component={CompanyList} />
+        <Stack.Screen name='CompanyDetails' component={CompanyDetails} />
+      </Stack.Navigator>
+    );
+  }
+
   return (
     <Drawer.Navigator
       drawerContent={props => <CustomDrawerContent {...props} />}
     >
       <Drawer.Screen
-        name='LocationList'
-        component={LocationList}
-        options={{
-          swipeEnabled: false,
-        }}
-      />
-      <Drawer.Screen name='CompanyList' component={CompanyList} />
-      <Drawer.Screen
-        name='CompanyDetails'
-        component={CompanyDetails}
-        options={{
-          swipeEnabled: false,
-        }}
-      />
-      <Drawer.Screen
-        name='About'
-        component={About}
-        options={{
-          swipeEnabled: false,
+        name='App'
+        component={StackScreens}
+        options={({ route }) => {
+          const currentScreen = route?.state?.routes[route?.state?.index].name;
+          return {
+            swipeEnabled: currentScreen === 'CompanyList',
+          };
         }}
       />
     </Drawer.Navigator>
