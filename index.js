ce13e20a7ad561ff86971f75ac26855109c3ce39
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-04-15 01:27:47 -0300

    Add some libs and update some configs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import 'react-native-gesture-handler';
 YellowBox.ignoreWarnings([
   'Non-serializable values were found in the navigation state',
   'Unable to find module for UIManager',
+  `Can't perform a React state update on an unmounted component.`,
 ]);
 
 require('react-native').unstable_enableLogBox();
